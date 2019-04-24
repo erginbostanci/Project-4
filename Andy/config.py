0e2497c67e18ca4db6901b9aaf4f6834c61efe5a
erginbostanci <erginbostanci@gmail.com>
2019-04-24 09:57:03 -0500

    Project-4

---
+++ Andy/config.py
@@ -0,0 +1,8 @@
+# Enter your API key
+# API: NCAAMB Trial
+# old API Key: API KEY: xb62p9vmshdq46tqyskbkcgu
+# new API KEY: mg9jrv2qw98ne5hkjybxa889
+# new again - not ready yet - API KEY: 9z3h7hcahqactppwxz8v36mb
+# new api - a@y Key: nn7mcjdxcjj92d9xw72xxm23
+
+api_key = "nn7mcjdxcjj92d9xw72xxm23"
---
+++ Collin/config.py
@@ -0,0 +1,5 @@
+# Enter your API key
+# API: NCAAMB Trial
+# API Key: API KEY: xb62p9vmshdq46tqyskbkcgu
+
+api_key="xb62p9vmshdq46tqyskbkcgu"
